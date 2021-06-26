be6bd793ac182de009143759a772d9928920e650
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-26 08:27:25 -0400

    modify ExampleTwo class: find count; separate string with comas

--- src/ExampleTwo.java
+++ src/ExampleTwo.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class ExampleTwo {
 	
 	public static void main(String[] args) {
-		String numbers = "1 2 35 79";
+		String numbers = "123456789";
+		String comaSeparated = "";
+		int count = 0;
+		
 		Scanner scanner = new Scanner(numbers);
+		scanner.useDelimiter("");
 		
 		while(scanner.hasNextInt()) {
-			System.out.println(scanner.nextInt());
+			int nextNum = scanner.nextInt();
+			
+			count += nextNum;
+			comaSeparated += nextNum;
+			
+			if(scanner.hasNextInt()) comaSeparated += ",";
 		}
 		
+		System.out.println("comaSeparated: " + comaSeparated);
+		System.out.println("Count: " + count);
 		
 		scanner.close();
 	}
